3e6c163878ebac1fdde13a1d3bc63b55a27e917d
wlfewn <582948588@qq.com>
2018-10-13 14:16:24 +0800

    通过模块分离到该仓库

---
+++ common-redis/src/main/java/com/github/geng/redis/entity/RedisModel.java
@@ -0,0 +1,14 @@
+package com.github.geng.redis.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
+
+/**
+ * 当redisTemple约束为类时，作为约束的父类
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RedisModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+}
